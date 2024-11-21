7b040b84efc427a4c8bb26ff7a1909cab858230a
pcwang （麓山老将） <wang_wang_lao@163.com>
2024-11-21 09:21:02 +0800

    hide email send password

--- sqlcarto/test/test.sql
+++ sqlcarto/test/test.sql
@@ -16,12 +16,12 @@ create extension postgis_sqlcarto;
 --     '测试一下',
 --     '这是一个从postgresql sqlcarto扩展发送过来的测试邮件',
 --     'smtps://smtp.126.com:465',
---     'SCUGOXHGWAEZUEQH'
+--     'XXXXXXXXXXXXX'
 -- );
 
 -- update sc_configuration set key_value = 'sqlcartotest@126.com' where key_name = 'EMAIL_USER';
 -- update sc_configuration set key_value = 'smtps://smtp.126.com:465' where key_name = 'EMAIL_SMTP';
--- update sc_configuration set key_value = 'SCUGOXHGWAEZUEQH' where key_name = 'EMAIL_PASSWORD';
+-- update sc_configuration set key_value = 'XXXXXXXXXXX' where key_name = 'EMAIL_PASSWORD';
 -- select sc_send_mail(
 --     'sqlcartotest@126.com',
 --     '测试一下',
@@ -33,21 +33,7 @@ create extension postgis_sqlcarto;
 -- select sc_generate_token(256);
 
 
--- create or replace function test_sqlcarto() returns text as 
--- $$
--- declare
---     maphandler text;
--- begin
---     maphandler := sc_canvas_create();
---     raise notice '%',maphandler;
---     maphandler := sc_canvas_draw_geometry(maphandler,'POINT(0 0)'::geometry);
---     maphandler := sc_canvas_save_to_file(maphandler, 'e:/abc.png','png');
---     maphandler := sc_canvas_destroy(maphandler);
---     return 'Has been destroyed';
--- end;
--- $$ LANGUAGE 'plpgsql';
 
 
--- \timing
--- select test_sqlcarto();
--- \timing
+
+
